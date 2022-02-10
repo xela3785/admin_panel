dc64463bc01812b3e9fbd82a9ed8aa5712f110b0
xela3785 <46381177+xela3785@users.noreply.github.com>
2022-02-11 03:12:02 +0700

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,56 @@
+const Orders = [
+    {
+        productName: 'Мини дрон',
+        productNumber: '54895',
+        paymentStatus: 'Ожидается оплата',
+        shipping: 'Ожидается отправка'
+    },
+    {
+        productName: 'AMD Ryzen 5 3600',
+        productNumber: '85631',
+        paymentStatus: 'Возврат средств',
+        shipping: 'Отменен'
+    },
+    {
+        productName: 'Palit NVIDIA GeForce RTX 3050',
+        productNumber: '12384',
+        paymentStatus: 'Оплачен',
+        shipping: 'Ожидается'
+    },
+    {
+        productName: 'GIGABYTE Z590M',
+        productNumber: '95148',
+        paymentStatus: 'Оплачен',
+        shipping: 'Доставлен'
+    },
+    {
+        productName: 'STINOL STS 185 S',
+        productNumber: '48903',
+        paymentStatus: 'Оплачен',
+        shipping: 'Доставлен'
+    },
+    {
+        productName: 'Logitech M185',
+        productNumber: '93148',
+        paymentStatus: 'Оплачен',
+        shipping: 'Доставлен'
+    },
+    {
+        productName: 'KitFort КТ-1654',
+        productNumber: '25794',
+        paymentStatus: 'Оплачен',
+        shipping: 'Доставлен'
+    },
+    {
+        productName: 'HYPERX Cloud Stinger Core',
+        productNumber: '33846',
+        paymentStatus: 'Оплачен',
+        shipping: 'Доставлен'
+    },
+    {
+        productName: 'AOC Value Line 27B2H/EU 27',
+        productNumber: '55721',
+        paymentStatus: 'Оплачен',
+        shipping: 'Доставлен'
+    },
+]
